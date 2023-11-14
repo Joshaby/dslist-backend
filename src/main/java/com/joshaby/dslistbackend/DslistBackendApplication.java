5734e0476ccd94113289adde5cff3eb0d85e4792
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2023-11-14 18:13:53 -0300

    Project created

---
+++ src/main/java/com/joshaby/dslistbackend/DslistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.joshaby.dslistbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joshaby/dslistbackend/DslistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshaby.dslistbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
